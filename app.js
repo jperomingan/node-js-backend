e573cef90cbb956ba7ca7ad7584071dbfa574d27
jperomingan@startechup.com <jperomingan@startechup.com>
2021-05-31 13:07:29 +0800

    Declaring variables and Assigning values to multiple variables

---
+++ app.js
@@ -0,0 +1,24 @@
+// Declaring variables
+// Longhand
+// let x;
+// let y = 20;
+
+// Shorthand
+let x,
+  y = 20;
+
+function sayHello(name) {
+  console.log("Hello" + name);
+}
+
+sayHello(y);
+
+// Longhand
+// let a, b, c;
+// a = 5;
+// b = 8;
+// c = 12;
+
+//Shorthand
+let [a, b, c] = [5, 8, 12];
+console.log(a, b, c);
