77f5ce5efda85204f82d092a6c7b2bbcae0dd895
jperomingan@startechup.com <jperomingan@startechup.com>
2021-06-02 11:20:41 +0800

    Arrow Function => in ES6

--- app.js
+++ app.js
@@ -1,272 +1,328 @@
-// 1.)  Declaring variables
-// Longhand
-// let x;
-// let y = 20;
+// // 1.)  Declaring variables
+// // Longhand
+// // let x;
+// // let y = 20;
 
-const { object } = require("firebase-functions/lib/providers/storage");
+// const { object } = require("firebase-functions/lib/providers/storage");
 
-// Shorthand
-let x,
-  y = 20;
+// // Shorthand
+// let x,
+//   y = 20;
 
-function sayHello(name) {
-  console.log("Hello" + name);
-}
-
-sayHello(y);
-
-// 2.) Assigning values to multiple variables
-// Longhand
-// let a, b, c;
-// a = 5;
-// b = 8;
-// c = 12;
-
-//Shorthand
-let [a, b, c] = [5, 8, 12];
-console.log(a, b, c);
-
-// 3.) The Ternary operator
-// Longhand
-// let marks = 26;
-// let result;
-// if (marks >= 30) {
-//   result = 'Pass';
-// } else {
-//   result = 'Fail';
+// function sayHello(name) {
+//   console.log("Hello" + name);
 // }
 
-// Shorthand
-let marks = 26;
-let result = marks >= 30 ? "Pass" : "Fail";
-console.log(result);
+// sayHello(y);
 
-// 4.) Assigning default value
-//Longhand
-// let imagePath;
-// let path = getImagePath();
-// // if(path !== null && path !== undefined && path !== '') {
-// //   imagePath = path;
+// // 2.) Assigning values to multiple variables
+// // Longhand
+// // let a, b, c;
+// // a = 5;
+// // b = 8;
+// // c = 12;
+
+// //Shorthand
+// let [a, b, c] = [5, 8, 12];
+// console.log(a, b, c);
+
+// // 3.) The Ternary operator
+// // Longhand
+// // let marks = 26;
+// // let result;
+// // if (marks >= 30) {
+// //   result = 'Pass';
 // // } else {
-// //   imagePath = 'default.jpg';
+// //   result = 'Fail';
 // // }
 
 // // Shorthand
-// let imagePath = getImagePath() || "default.jpg";
-// console.log(imagePath);
+// let marks = 26;
+// let result = marks >= 30 ? "Pass" : "Fail";
+// console.log(result);
 
-// 5.) AND(&&) Short circuit evaluation
-// Longhand
-let isLoggedin;
-if (isLoggedin) {
-  goToHomePage();
-}
+// // 4.) Assigning default value
+// //Longhand
+// // let imagePath;
+// // let path = getImagePath();
+// // // if(path !== null && path !== undefined && path !== '') {
+// // //   imagePath = path;
+// // // } else {
+// // //   imagePath = 'default.jpg';
+// // // }
 
-// Shorthand
-isLoggedin && goToHomePage();
+// // // Shorthand
+// // let imagePath = getImagePath() || "default.jpg";
+// // console.log(imagePath);
 
-console.log(isLoggedin && goToHomePage());
-
-// 6.) Swap two variables
-// let c = "Hello",
-d = 55;
-
-// Longhand
-// const temp = x;
-// x = y;
-// y = temp;
-
-// Shorthand
-[c, d] = [d, c];
-
-console.log(c, d);
-
-// 7.) Arrow Function
-// Longhand
-// function add(num1, num2) {
-//   return num1 + num2;
+// // 5.) AND(&&) Short circuit evaluation
+// // Longhand
+// let isLoggedin;
+// if (isLoggedin) {
+//   goToHomePage();
 // }
 
-//Shorthand
-const add = (num1, num2) => num1 + num2;
+// // Shorthand
+// isLoggedin && goToHomePage();
 
-console.log(add(4, 5));
+// console.log(isLoggedin && goToHomePage());
 
-// 8.) Template Literals
-let number = 3;
-let time = "2pm";
-// Longhand
-console.log("You got a missed call from " + number + " at " + time);
+// // 6.) Swap two variables
+// // let c = "Hello",
+// d = 55;
 
-// Shorthand
-console.log("You got a missed call from " + number + " at " + time);
+// // Longhand
+// // const temp = x;
+// // x = y;
+// // y = temp;
 
-// 9.) Multi-line String
+// // Shorthand
+// [c, d] = [d, c];
 
-// Longhand
-console.log(
-  "JavaScript, often abbreviated as JS, is a\n" +
-    "programming language that conforms to the \n" +
-    "ECMAScript specification. JavaScript is high-level, \n" +
-    "often just-in-time compiled, and multi-paradigm"
-);
+// console.log(c, d);
 
-//Shorthand
-console.log(`JavaScript, often abbreviated as JS, is a 
-  programming language that conforms to the ECMAScript specification. JavaScript 
-  is high-level, often just-in-time compiled, and multi-paradigm.`);
+// // 7.) Arrow Function
+// // Longhand
+// // function add(num1, num2) {
+// //   return num1 + num2;
+// // }
 
-// 10.) Multiple condition checking
-// Longhand
-let value = 1;
-if (value === 1 || value === "one" || value === 2 || vaue === "two") {
-  // Execute some code
-}
+// //Shorthand
+// const add = (num1, num2) => num1 + num2;
 
-// Shorthand 1
-if ([1, "one", 2, "two"].indexOf(value) >= 0) {
-  // Execute some code
-  console.log("Jennelyn shorthand 1");
-}
+// console.log(add(4, 5));
 
-// Shorthand 2
-if ([1, "one", 2, "two"].includes(value)) {
-  // Execute some code
-  console.log("Jennelyn shorthand 2");
-}
+// // 8.) Template Literals
+// let number = 3;
+// let time = "2pm";
+// // Longhand
+// console.log("You got a missed call from " + number + " at " + time);
 
-// 11.) Object Property Assignment
-let firstname = "Amitav";
-let lastname = "Mishra";
+// // Shorthand
+// console.log("You got a missed call from " + number + " at " + time);
 
-// Longhand
-let obj1 = { firstname: firstname, lastname: lastname };
-console.log(obj1);
+// // 9.) Multi-line String
 
-// Shorthand
-let obj2 = { firstname, lastname };
-console.log(obj2);
+// // Longhand
+// console.log(
+//   "JavaScript, often abbreviated as JS, is a\n" +
+//     "programming language that conforms to the \n" +
+//     "ECMAScript specification. JavaScript is high-level, \n" +
+//     "often just-in-time compiled, and multi-paradigm"
+// );
 
-// 12.) String into a Number
-// Longhand
-// let total = parseInt("453");
-// let average = parseFloat("42.6");
+// //Shorthand
+// console.log(`JavaScript, often abbreviated as JS, is a
+//   programming language that conforms to the ECMAScript specification. JavaScript
+//   is high-level, often just-in-time compiled, and multi-paradigm.`);
 
-// Shorthand
-let total = +"453";
-let average = +"42.6";
+// // 10.) Multiple condition checking
+// // Longhand
+// let value = 1;
+// if (value === 1 || value === "one" || value === 2 || vaue === "two") {
+//   // Execute some code
+// }
 
-console.log(total, average);
+// // Shorthand 1
+// if ([1, "one", 2, "two"].indexOf(value) >= 0) {
+//   // Execute some code
+//   console.log("Jennelyn shorthand 1");
+// }
 
-// 13.) Repeat a string for multiple times
-// Longhand
-let str = "";
-for (let i = 0; i < 5; i++) {
-  str += "Hello ";
-}
-console.log(str); //
+// // Shorthand 2
+// if ([1, "one", 2, "two"].includes(value)) {
+//   // Execute some code
+//   console.log("Jennelyn shorthand 2");
+// }
 
-// Shorthand
-"Hello ".repeat(5);
-console.log("Hello ".repeat(5));
+// // 11.) Object Property Assignment
+// let firstname = "Amitav";
+// let lastname = "Mishra";
 
-// 14.) Exponent Power
-// Longhand
-const power1 = Math.pow(4, 3); // 64
+// // Longhand
+// let obj1 = { firstname: firstname, lastname: lastname };
+// console.log(obj1);
 
-// Shorthand
-const power2 = 4 ** 3; // 64
+// // Shorthand
+// let obj2 = { firstname, lastname };
+// console.log(obj2);
 
-console.log(power1, power2);
+// // 12.) String into a Number
+// // Longhand
+// // let total = parseInt("453");
+// // let average = parseFloat("42.6");
 
-// 15.) Double NOT bitwise operator (~ ~)
-// Longhand
-const floor1 = Math.floor(6.8); //6
+// // Shorthand
+// let total = +"453";
+// let average = +"42.6";
 
-// Shorthand
-const floor2 = ~~6.8; // 6
+// console.log(total, average);
 
-console.log(floor1, floor2);
+// // 13.) Repeat a string for multiple times
+// // Longhand
+// let str = "";
+// for (let i = 0; i < 5; i++) {
+//   str += "Hello ";
+// }
+// console.log(str); //
 
-// 16.) Find max and min number in array
-// Shorthand
-const arr = [2, 8, 15, 4];
-Math.max(...arr); // 15
-Math.min(...arr); // 2
+// // Shorthand
+// "Hello ".repeat(5);
+// console.log("Hello ".repeat(5));
 
-console.log(arr, Math.max(...arr), Math.min(...arr));
+// // 14.) Exponent Power
+// // Longhand
+// const power1 = Math.pow(4, 3); // 64
 
-// 17.) For loop
-let arr1 = [10, 20, 30, 40];
+// // Shorthand
+// const power2 = 4 ** 3; // 64
 
-// Longhand
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-}
+// console.log(power1, power2);
 
-// Shorthand
-// for of loop
-for (const val of arr) {
-  console.log(val);
-}
+// // 15.) Double NOT bitwise operator (~ ~)
+// // Longhand
+// const floor1 = Math.floor(6.8); //6
 
-// for in loop
-for (const index in arr) {
-  console.log(`index: ${index} and value: ${arr[index]}`);
-}
+// // Shorthand
+// const floor2 = ~~6.8; // 6
 
-let obj3 = { x: 20, y: 50 };
-for (const key in obj3) {
-  console.log(obj3[key]);
-}
+// console.log(floor1, floor2);
 
-// 18.) Merging of arrays
-let arr2 = [20, 30];
+// // 16.) Find max and min number in array
+// // Shorthand
+// const arr = [2, 8, 15, 4];
+// Math.max(...arr); // 15
+// Math.min(...arr); // 2
 
-// Longhand
-let arr3 = arr2.concat([60, 80]);
-// [20, 30, 60, 80]
+// console.log(arr, Math.max(...arr), Math.min(...arr));
 
-// Shorthand
-let arr4 = [...arr2, 60, 80];
-// [20, 30, 60, 80]
+// // 17.) For loop
+// let arr1 = [10, 20, 30, 40];
 
-console.log(arr2, arr3);
+// // Longhand
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
 
-// 19.) Deep cloning of multi-level object
-let obj = { x: 20, y: { z: 30 } };
+// // Shorthand
+// // for of loop
+// for (const val of arr) {
+//   console.log(val);
+// }
 
-//Longhand
-const makeDeepClone = (obj) => {
-  let newObject = {};
-  Object.keys(obj).map((key) => {
-    if (typeof obj[key] === "object") {
-      newObject[key] = makeDeepClone(obj[key]);
-    } else {
-      newObject[key] = obj[key];
-    }
-  });
-  return newObject;
+// // for in loop
+// for (const index in arr) {
+//   console.log(`index: ${index} and value: ${arr[index]}`);
+// }
+
+// let obj3 = { x: 20, y: 50 };
+// for (const key in obj3) {
+//   console.log(obj3[key]);
+// }
+
+// // 18.) Merging of arrays
+// let arr2 = [20, 30];
+
+// // Longhand
+// let arr3 = arr2.concat([60, 80]);
+// // [20, 30, 60, 80]
+
+// // Shorthand
+// let arr4 = [...arr2, 60, 80];
+// // [20, 30, 60, 80]
+
+// console.log(arr2, arr3);
+
+// // 19.) Deep cloning of multi-level object
+// let obj = { x: 20, y: { z: 30 } };
+
+// //Longhand
+// const makeDeepClone = (obj) => {
+//   let newObject = {};
+//   Object.keys(obj).map((key) => {
+//     if (typeof obj[key] === "object") {
+//       newObject[key] = makeDeepClone(obj[key]);
+//     } else {
+//       newObject[key] = obj[key];
+//     }
+//   });
+//   return newObject;
+// };
+
+// const cloneObj1 = makeDeepClone(obj);
+
+// // Shorthand
+// const cloneObj2 = JSON.parse(JSON.stringify(obj));
+
+// // Shorthand for single level object
+// let obj4 = { x: 20, y: "hello" };
+// const cloneObj3 = { ...obj4 };
+
+// console.log(cloneObj1, cloneObj2, cloneObj3);
+
+// // 20.) Get character from string
+// let str1 = "jscurious.com";
+
+// // Longhand
+// str1.charAt(2);
+
+// // Shorthand
+// str1[2];
+
+// console.log(str[2]);
+
+// const sayHello = (name) => console.log("Hello " + name);
+// sayHello("Jennelyn");
+
+// console.log(); // global
+
+// setTimeout();
+// clearTimeout();
+
+// setInterval();
+// clearInterval();
+
+// var message = "";
+
+// // console.log(module);
+
+// var logger = require("./logger");
+
+// logger.log("message");
+
+// const http = require("http");
+// const hostname = "127.0.0.1";
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello Jennelyn");
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+// Arrow Function => in ES6
+
+var createGreeting = function (message, name) {
+  return message + name;
 };
 
-const cloneObj1 = makeDeepClone(obj);
+var arrowGreeting = (name) => "Jennelyn";
+console.log(arrowGreeting());
 
-// Shorthand
-const cloneObj2 = JSON.parse(JSON.stringify(obj));
+var deliveryBoy = {
+  name: "Jennelyn",
+  handleMessage: function (message, handler) {
+    handler(message);
+  },
+  receive: function () {
+    this.handleMessage("Hello, ", (message) =>
+      console.log(message + this.name)
+    );
+  },
+};
 
-// Shorthand for single level object
-let obj4 = { x: 20, y: "hello" };
-const cloneObj3 = { ...obj4 };
-
-console.log(cloneObj1, cloneObj2, cloneObj3);
-
-// 20.) Get character from string
-let str1 = "jscurious.com";
-
-// Longhand
-str1.charAt(2);
-
-// Shorthand
-str1[2];
-
-console.log(str[2]);
+deliveryBoy.receive();
